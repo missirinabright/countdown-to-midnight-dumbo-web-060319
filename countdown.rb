df7b16d554bc07259d032982b6ee71581b1badc9
Irina Bright <missirinabright@gmail.com>
2019-05-17 16:50:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  num = 1
+  while num < 10
+  puts "#{num} SECOND(S)!"
+  num -= 1 
+  end
+"HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num)
+  
+end
+
